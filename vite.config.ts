f03010f2f47fc26fb5462f60bed5d300e7642899
lakshmi1239 <lakshmikovela42@gmail.com>
2025-05-30 21:46:07 +0530

    Creating portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Vara_portfolio/', // ✅ This line is crucial!
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
